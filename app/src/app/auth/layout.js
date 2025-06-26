9fee7d5e74708a061346fbbdbf5fe1ff7297a589
Heithor <johanefelizu@gmail.com>
2025-06-25 22:36:12 -0400

    Added shared layout for signup process and login

--- app/src/app/auth/layout.js
+++ app/src/app/auth/layout.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import Image from "next/image";
 
 export const metadata = {
   title: "Auth",
@@ -8,7 +9,21 @@ export const metadata = {
 export default function AuthLayout({ children }) {
   return (
     <html lang="es">
-      <body className="flex justify-center items-center w-screen h-screen px-32 py-12">{children}</body>
+      <body className="flex justify-center items-center w-screen h-screen px-32 py-12">
+        <div className="login-container relative flex flex-row justify-center items-center bg-white w-full h-full shadow-xl rounded-2xl">
+          <div className="left-column flex flex-col gap-8  justify-center items-center w-3/4 h-full p-5">{children}</div>
+
+          <div className="right-column bg-[url(/imagenes/login1.jpg)] bg-cover bg-no-repeat bg-center w-1/2 h-full border rounded-tr-2xl rounded-br-2xl relative flex flex-col justify-start items-center">
+            <div
+              className="absolute rounded-tr-2xl rounded-br-2xl inset-0 pointer-events-none z-1"
+              style={{ background: "radial-gradient(ellipse at center, transparent 20%, rgba(0,0,0,0.5) 100%)" }}
+            ></div>
+            <div className="logo relative z-2 py-10">
+              <Image width={104} height={40} className="h-10 w-26 aspect-video" src="/logos/logotipo_w.png" alt="LogoTipo Trivo" />
+            </div>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
