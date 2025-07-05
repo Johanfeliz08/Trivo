438f15fd11796cb13a23789793fad965f6a8c8cf
Heithor <johanefelizu@gmail.com>
2025-07-05 02:40:01 -0400

    Added nextjs middleware to handle routes protection and authentication

---
+++ app/src/middleware.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+/**
+ * @param {import('next/server').NextRequest} request
+ */
+
+export function middleware(request) {
+  const token = request.cookies.get("tokenAcceso");
+
+  const isAuthRoute = request.nextUrl.pathname.startsWith("/auth");
+  const isProtected = request.nextUrl.pathname.startsWith("/home");
+
+  if (isProtected && !token) {
+    const loginUrl = new URL("/auth/login", request.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  if (isAuthRoute && token) {
+    const homeUrl = new URL("/home/feed", request.url);
+    return NextResponse.redirect(homeUrl);
+  }
+
+  return NextResponse.next();
+}
+
+// A qué rutas aplicar el middleware
+export const config = {
+  matcher: ["/home/:path*", "/auth/:path*"],
+};
