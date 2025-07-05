549ca67a289695ff0e95049d71facdfa60ad7a82
Heithor <johanefelizu@gmail.com>
2025-07-04 23:33:52 -0400

    Added logic for fetching data, code validation and fixed zod validation

--- app/src/app/auth/signup/steps/step4.jsx
+++ app/src/app/auth/signup/steps/step4.jsx
@@ -1,14 +1,22 @@
 import * as z from "zod/v4";
 import { useState, useRef, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import api from "@/lib/api/api";
+import Loader from "@/components/ui/loader";
+import Modal from "@/components/ui/modal";
 
 export default function Step4({ currentStep, setCurrentStep, userData, setUserData }) {
+  const router = useRouter();
+
+  const digitSchema = z.string().regex(/^\d{1}$/, "Debe ser un número de un solo dígito");
+
   const confirmationCodeSchema = z.object({
-    firstDigit: z.string().length(1),
-    secondDigit: z.string().length(1),
-    thirdDigit: z.string().length(1),
-    fourthDigit: z.string().length(1),
-    fifthDigit: z.string().length(1),
-    sixthDigit: z.string().length(1),
+    firstDigit: digitSchema,
+    secondDigit: digitSchema,
+    thirdDigit: digitSchema,
+    fourthDigit: digitSchema,
+    fifthDigit: digitSchema,
+    sixthDigit: digitSchema,
   });
 
   const [confirmationCode, setConfirmationCode] = useState({
@@ -20,7 +28,18 @@ export default function Step4({ currentStep, setCurrentStep, userData, setUserDa
     sixthDigit: "",
   });
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const isCodeValid = confirmationCodeSchema.safeParse(confirmationCode).success;
+
   const [currentDigit, setCurrentDigit] = useState(1);
+  const [errors, setErrors] = useState({
+    confirmationCode: {
+      error: false,
+      message: "",
+    },
+  });
 
   const firstDigitRef = useRef(null);
   const secondDigitRef = useRef(null);
@@ -35,9 +54,63 @@ export default function Step4({ currentStep, setCurrentStep, userData, setUserDa
     digitsRefs[currentDigit - 1].current?.focus();
   }, [currentDigit]);
 
+  const handleConfirmationCode = async (e) => {
+    e.preventDefault();
+    try {
+      setIsLoading(true);
+      const parsedCode = confirmationCodeSchema.safeParse(confirmationCode);
+      if (!parsedCode.success) {
+        setErrors({
+          confirmationCode: {
+            error: true,
+            message: "El codigo de confirmación no es válido. Por favor, verifique e intente nuevamente.",
+          },
+        });
+        return;
+      }
+
+      const code = Object.values(parsedCode.data).join("");
+      const res = await api.post(`/users/confirm-account?usuarioId=${userData.usuarioId}&&codigo=${code}`);
+
+      if (res.status === 200) {
+        setErrors({
+          confirmationCode: {
+            error: false,
+            message: "",
+          },
+        });
+
+        setShowModal(true);
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        setErrors({
+          confirmationCode: {
+            error: true,
+            message: "Código de confirmación inválido. Por favor, inténtalo de nuevo.",
+          },
+        });
+        setCurrentDigit(1);
+        setConfirmationCode({
+          firstDigit: "",
+          secondDigit: "",
+          thirdDigit: "",
+          fourthDigit: "",
+          fifthDigit: "",
+          sixthDigit: "",
+        });
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       <div className="step-container flex flex-col justify-center items-center gap-18 h-full w-full">
+        {isLoading && <Loader />}
+        {showModal && <Modal message="Su cuenta ha sido creada satisfactoriamente, por favor inicie sesion." redirectTo="/auth/login" type="success" />}
+
         <div className="title flex justify-center items-center flex-col gap-4">
           <h1 className="text-3xl font-semibold text-primary">¡Confirma tu cuenta!</h1>
           <p className="font-regular opacity-60 text-md w-1/2 text-justify">
@@ -45,7 +118,7 @@ export default function Step4({ currentStep, setCurrentStep, userData, setUserDa
           </p>
         </div>
 
-        <div className="confirmation-code">
+        <div className="confirmation-code flex flex-col justify-center items-center gap-4">
           <form action="" className="flex flex-row justify-center items-center gap-4">
             <div className="first-digit">
               <input
@@ -56,14 +129,17 @@ export default function Step4({ currentStep, setCurrentStep, userData, setUserDa
                     `}
                 ref={firstDigitRef}
                 autoFocus
-                onKeyUp={(e) => {
+                onChange={(e) => {
                   setConfirmationCode({ ...confirmationCode, firstDigit: e.target.value });
+                }}
+                onKeyUp={(e) => {
                   if (e.key === "Backspace") {
                     setCurrentDigit(1);
                   } else {
                     setCurrentDigit(2);
                   }
                 }}
+                value={confirmationCode.firstDigit}
               />
             </div>
             <div className="second-digit">
@@ -73,15 +149,17 @@ export default function Step4({ currentStep, setCurrentStep, userData, setUserDa
                 maxLength="1"
                 className="w-18 h-18 text-center text-4xl bg-bg-secondary text-primary border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-primary"
                 ref={secondDigitRef}
-                onKeyUp={(e) => {
+                onChange={(e) => {
                   setConfirmationCode({ ...confirmationCode, secondDigit: e.target.value });
-
+                }}
+                onKeyUp={(e) => {
                   if (e.key === "Backspace") {
                     setCurrentDigit(1);
                   } else {
                     setCurrentDigit(3);
                   }
                 }}
+                value={confirmationCode.secondDigit}
               />
             </div>
             <div className="third-digit">
@@ -91,14 +169,17 @@ export default function Step4({ currentStep, setCurrentStep, userData, setUserDa
                 maxLength="1"
                 className="w-18 h-18 text-center text-4xl bg-bg-secondary text-primary border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-primary"
                 ref={thirdDigitRef}
-                onKeyUp={(e) => {
+                onChange={(e) => {
                   setConfirmationCode({ ...confirmationCode, thirdDigit: e.target.value });
+                }}
+                onKeyUp={(e) => {
                   if (e.key === "Backspace") {
                     setCurrentDigit(2);
                   } else {
                     setCurrentDigit(4);
                   }
                 }}
+                value={confirmationCode.thirdDigit}
               />
             </div>
             <div className="fourth-digit">
@@ -108,14 +189,17 @@ export default function Step4({ currentStep, setCurrentStep, userData, setUserDa
                 maxLength="1"
                 className="w-18 h-18 text-center text-4xl bg-bg-secondary text-primary border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-primary"
                 ref={fourthDigitRef}
-                onKeyUp={(e) => {
+                onChange={(e) => {
                   setConfirmationCode({ ...confirmationCode, fourthDigit: e.target.value });
+                }}
+                onKeyUp={(e) => {
                   if (e.key === "Backspace") {
                     setCurrentDigit(3);
                   } else {
                     setCurrentDigit(5);
                   }
                 }}
+                value={confirmationCode.fourthDigit}
               />
             </div>
             <div className="fifth-digit">
@@ -125,14 +209,17 @@ export default function Step4({ currentStep, setCurrentStep, userData, setUserDa
                 maxLength="1"
                 className="w-18 h-18 text-center text-4xl bg-bg-secondary text-primary border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-primary"
                 ref={fifthDigitRef}
-                onKeyUp={(e) => {
+                onChange={(e) => {
                   setConfirmationCode({ ...confirmationCode, fifthDigit: e.target.value });
+                }}
+                onKeyUp={(e) => {
                   if (e.key === "Backspace") {
                     setCurrentDigit(4);
                   } else {
                     setCurrentDigit(6);
                   }
                 }}
+                value={confirmationCode.fifthDigit}
               />
             </div>
             <div className="sixth-digit">
@@ -142,20 +229,28 @@ export default function Step4({ currentStep, setCurrentStep, userData, setUserDa
                 maxLength="1"
                 className="w-18 h-18 text-center text-4xl bg-bg-secondary text-primary border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-primary"
                 ref={sixthDigitRef}
-                onKeyUp={(e) => {
+                onChange={(e) => {
                   setConfirmationCode({ ...confirmationCode, sixthDigit: e.target.value });
+                }}
+                onKeyUp={(e) => {
                   if (e.key === "Backspace") {
-                    setCurrentDigit(5); // Go back to the fourth digit if backspace is pressed
+                    setCurrentDigit(5);
+                  } else {
+                    setCurrentDigit(6);
                   }
                 }}
+                value={confirmationCode.sixthDigit}
               />
             </div>
           </form>
+          <div className="no-data flex justify-center items-center">
+            <p className="text-center text-red-500">{errors.confirmationCode ? errors.confirmationCode.message : ""}</p>
+          </div>
         </div>
 
         <div className="buttons flex flex-row-reverse justify-between items-center">
           <div className="next-btn relative">
-            <button className="cursor-pointer" onClick={() => {}} disabled={!confirmationCodeSchema.safeParse(confirmationCode).success}>
+            <button className="cursor-pointer" onClick={handleConfirmationCode} disabled={!isCodeValid}>
               <div className="text px-10">
                 <span className="main-text font-semibold"></span>
                 <span className="secondary-text font-light">Continuar</span>
