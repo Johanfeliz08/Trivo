e75f102db84e2e102545bf8d06c0f51a63a9c282
unknown <johanefelizu@gmail.com>
2025-07-05 20:11:41 -0400

    Disabled routes protection to test faster and deleted unnecesary files

--- app/src/api/intereses/route.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import intereses from "@/lib/mock_data/intereses.json";
-
-export async function GET(request) {
-  return NextResponse.json(intereses);
-}
--- app/src/middleware.js
+++ app/src/middleware.js
@@ -7,23 +7,24 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   const token = request.cookies.get("tokenAcceso");
 
-  const isAuthRoute = request.nextUrl.pathname.startsWith("/auth");
+  // const isAuthRoute = request.nextUrl.pathname.startsWith("/auth");
   const isProtected = request.nextUrl.pathname.startsWith("/home");
 
-  if (isProtected && !token) {
-    const loginUrl = new URL("/auth/login", request.url);
-    return NextResponse.redirect(loginUrl);
-  }
+  // if (isProtected && !token) {
+  //   const loginUrl = new URL("/auth/login", request.url);
+  //   return NextResponse.redirect(loginUrl);
+  // }
 
-  if (isAuthRoute && token) {
-    const homeUrl = new URL("/home/feed", request.url);
-    return NextResponse.redirect(homeUrl);
-  }
+  // if (isAuthRoute && token) {
+  //   const homeUrl = new URL("/home/feed", request.url);
+  //   return NextResponse.redirect(homeUrl);
+  // }
 
   return NextResponse.next();
 }
 
 // A qué rutas aplicar el middleware
 export const config = {
-  matcher: ["/home/:path*", "/auth/:path*"],
+  // matcher: ["/home/:path*", "/auth/:path*"],
+  matcher: ["/home/:path*"],
 };
