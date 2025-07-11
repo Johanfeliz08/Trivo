aa6b16c0ee7a62b00c448067da9aa890c89edc78
Heithor <johanefelizu@gmail.com>
2025-07-11 01:59:16 -0400

    Added user data and complete layout of the page

--- app/src/app/(home)/usuario/miperfil/page.js
+++ app/src/app/(home)/usuario/miperfil/page.js
@@ -1,24 +1,46 @@
+"use client";
+
 import Image from "next/image";
+import ProfileBanner from "@/components/ui/Usuario/MiPerfil/ProfileBanner";
+import AboutMe from "@/components/ui/Usuario/MiPerfil/AboutMe";
+import Skills from "@/components/ui/Usuario/MiPerfil/Skills";
+import Interests from "@/components/ui/Usuario/MiPerfil/Interests";
+import { useState } from "react";
 
 export default function MiPerfilPage() {
+  const [user, setUser] = useState({
+    nombre: "Johan Feliz",
+    profesion: "Ingeniero de Software",
+    estado: "Disponible",
+    ubicacion: "Barahona, Republica Dominicana",
+    imagen: "/imagenes/user.jpg",
+    biografia: "Desarrollador Full Stack con experiencia en Next.js y React.",
+    habilidades: ["JavaScript", "React", "Next.js", "Node.js", "CSS", "HTML"],
+    intereses: ["Tecnología", "Desarrollo Web", "Inteligencia Artificial", "Machine Learning"],
+  });
+
   return (
     <>
       <div className="MiPerfil-container w-full h-full xl:p-10 z-5">
         <div className="miperfil bg-white shadow-xl roundex-2xl w-full h-full flex flex-col">
           <div className="header w-full border-b border-gray-200 flex justify-between items-center px-15 py-10">
             <div className="title">
-              <h1 className="text-2xl font-semibold text-primary">Mi Perfil</h1>
+              <h1 className="text-3xl font-semibold text-primary">Mi Perfil</h1>
             </div>
-            <div className="close-btn">x</div>
-          </div>
-          <div className="content w-full h-full flex flex-col items-center justify-start px-20 py-10 overflow-scroll hide-scrollbar">
-            <div className="profile-header w-full">
-              <div className="banner w-full h-50 bg-primary flex items-center justify-center">
-                <div className="logo">
-                  <Image src={"/logos/isotipo_w.png"} width={800} height={800} alt="Trivo Isotipo" className="size-35 opacity-30" />
-                </div>
+            <div className="close-btn">
+              <div className="icon cursor-pointer">
+                <svg className="size-10 fill-primary hover:fill-black ease-in-out duration-400" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512">
+                  <path d="M18,6h0a1,1,0,0,0-1.414,0L12,10.586,7.414,6A1,1,0,0,0,6,6H6A1,1,0,0,0,6,7.414L10.586,12,6,16.586A1,1,0,0,0,6,18H6a1,1,0,0,0,1.414,0L12,13.414,16.586,18A1,1,0,0,0,18,18h0a1,1,0,0,0,0-1.414L13.414,12,18,7.414A1,1,0,0,0,18,6Z" />
+                </svg>
               </div>
-              <div className="user-profile"></div>
+            </div>
+          </div>
+          <div className="content w-full h-full flex flex-col items-start justify-start px-20 py-10 overflow-scroll hide-scrollbar">
+            <ProfileBanner user={user} />
+            <div className="details grid grid-cols-2 gap-4 w-full">
+              <AboutMe biografia={user.biografia} />
+              <Skills habilidades={user.habilidades} />
+              <Interests intereses={user.intereses} />
             </div>
           </div>
         </div>
