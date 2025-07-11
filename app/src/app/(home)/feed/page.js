0bc9adc4fc526ae555c6b5872f789ee3ec9de26f
Heithor <johanefelizu@gmail.com>
2025-07-10 20:23:23 -0400

    Added /home as group route to remove it from the url but keep the layout

--- app/src/app/(home)/feed/page.js
+++ app/src/app/(home)/feed/page.js
@@ -5,7 +5,7 @@ import ActionBar from "@/components/ui/Home/Feed/ActionBar";
 export default function feedPage() {
   return (
     <>
-      <div className="feed-container w-full h-full flex flex-col items-center justify-evenly 2xl:py-10 xl:py-5 xl:gap-4">
+      <div className="feed-container w-full h-full flex flex-col items-center justify-evenly overflow-hidden 2xl:py-10 xl:py-3 xl:gap-4">
         <TabBar />
         <Feed />
         <ActionBar />
