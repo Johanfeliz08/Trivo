8630d22d73b34b280c11f742df157446b3764efe
Heithor <johanefelizu@gmail.com>
2025-07-18 20:12:04 -0400

    Updated modal mi perfil to pass the userId of the current feed user and can now handle its open/close state

--- app/src/components/ui/Usuario/MiPerfil/ModalMiPerfil.jsx
+++ app/src/components/ui/Usuario/MiPerfil/ModalMiPerfil.jsx
@@ -1,88 +1,35 @@
 "use client";
 
-import Image from "next/image";
 import ProfileBanner from "@/components/ui/Usuario/MiPerfil/ProfileBanner";
 import AboutMe from "@/components/ui/Usuario/MiPerfil/AboutMe";
 import Skills from "@/components/ui/Usuario/MiPerfil/Skills";
 import Interests from "@/components/ui/Usuario/MiPerfil/Interests";
-import { useState, useEffect } from "react";
-import Loader from "@/components/ui/Loader";
-import Modal from "@/components/ui/Modal";
-import api from "@/lib/api/api";
-import Cookies from "js-cookie";
 
-export default function ModalMiPerfil() {
-  const [userData, setUserData] = useState({
-    nombre: "",
-    apellido: "",
-    biografia: "",
-    fotoPerfil: "",
-    ubicacion: "",
-    habilidad: [],
-    interes: [],
-  });
-  const [loading, setLoading] = useState(false);
-  const [errors, setErrors] = useState({
-    general: {
-      error: false,
-      message: "Error al cargar los datos del usuario. Por favor, vuelva a iniciar sesión.",
-    },
-  });
-
-  // Fetch user data from the API
-  const fetchUserData = async () => {
-    try {
-      const userId = Cookies.get("userId");
-      setLoading(true);
-
-      if (userId) {
-        const response = await api.get(`/users/profile/${userId}`);
-        if (response.status === 200) {
-          setUserData(response.data);
-        }
-      }
-    } catch (error) {
-      setErrors((prevErrors) => ({
-        ...prevErrors,
-        general: {
-          error: true,
-          message: "Error al cargar los datos del usuario. Por favor, vuelva a iniciar sesión.",
-        },
-      }));
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // Feth de user data when the component mounts
-  useEffect(() => {
-    fetchUserData();
-  }, []);
-
+export default function ModalMiPerfil({ userId, isPerfilModalOpen, setIsPerfilModalOpen }) {
   return (
     <>
-      {loading && <Loader />}
-      {errors.general.error && <Modal message={errors.general.message} redirectTo="/feed" type="error" logout={false} />}
-      <div className="MiPerfil-container w-full h-full xl:p-10 z-5">
+      <div className={`modalMiPerfil w-full h-full xl:p-10 z-100 absolute ${isPerfilModalOpen ? "modalPerfilOpen" : ""} transition-opacity duration-300 ease-in-out`}>
         <div className="miperfil bg-white shadow-xl roundex-2xl w-full h-full flex flex-col">
           <div className="header w-full border-b border-gray-200 flex justify-between items-center px-15 py-10">
             <div className="title">
-              <h1 className="text-3xl font-semibold text-primary">Mi Perfil</h1>
+              <h1 className="text-3xl font-semibold text-primary">Perfil</h1>
             </div>
             <div className="close-btn">
-              <div className="icon cursor-pointer">
-                <svg className="size-10 fill-primary hover:fill-black ease-in-out duration-400" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512">
-                  <path d="M18,6h0a1,1,0,0,0-1.414,0L12,10.586,7.414,6A1,1,0,0,0,6,6H6A1,1,0,0,0,6,7.414L10.586,12,6,16.586A1,1,0,0,0,6,18H6a1,1,0,0,0,1.414,0L12,13.414,16.586,18A1,1,0,0,0,18,18h0a1,1,0,0,0,0-1.414L13.414,12,18,7.414A1,1,0,0,0,18,6Z" />
-                </svg>
-              </div>
+              <button type="button" className="cursor-pointer" onClick={() => setIsPerfilModalOpen(false)}>
+                <div className="icon cursor-pointer">
+                  <svg className="size-10 fill-primary hover:fill-black ease-in-out duration-400" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512">
+                    <path d="M18,6h0a1,1,0,0,0-1.414,0L12,10.586,7.414,6A1,1,0,0,0,6,6H6A1,1,0,0,0,6,7.414L10.586,12,6,16.586A1,1,0,0,0,6,18H6a1,1,0,0,0,1.414,0L12,13.414,16.586,18A1,1,0,0,0,18,18h0a1,1,0,0,0,0-1.414L13.414,12,18,7.414A1,1,0,0,0,18,6Z" />
+                  </svg>
+                </div>
+              </button>
             </div>
           </div>
           <div className="content w-full h-full flex flex-col items-start justify-start px-20 py-10 overflow-scroll hide-scrollbar">
-            <ProfileBanner userData={userData} />
+            <ProfileBanner userIdProp={userId} />
             <div className="details grid grid-cols-2 gap-4 w-full">
-              <AboutMe biografia={userData.biografia} />
-              <Skills habilidades={userData.habilidad} />
-              <Interests intereses={userData.interes} />
+              <AboutMe userIdProp={userId} />
+              <Skills userIdProp={userId} />
+              <Interests userIdProp={userId} />
             </div>
           </div>
         </div>
