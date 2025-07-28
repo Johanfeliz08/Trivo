2d92d706bcc84b3204e1f26c1f625fc40f2cad4f
Heithor <johanefelizu@gmail.com>
2025-07-28 17:46:41 -0400

    Updated feed component to handle if the user is searching or is on the recommended users by ia

--- app/src/app/(home)/feed/page.js
+++ app/src/app/(home)/feed/page.js
@@ -3,17 +3,34 @@ import TabBar from "@/components/ui/Feed/TabBar";
 import Feed from "@/components/ui/Feed/Feed";
 import ActionBar from "@/components/ui/Feed/ActionBar";
 import { useState } from "react";
+import FeedByFilters from "@/components/ui/Feed/busqueda-por-filtros/FeedByFilters";
+import TabBarByFilters from "@/components/ui/Feed/busqueda-por-filtros/TabBarByFilters";
 
 export default function FeedPage() {
   const [currentTab, setCurrentTab] = useState("recomendados"); // Current tab for feed
   const [currentUserId, setCurrentUserId] = useState(null);
+  const [applyFilters, setApplyFilters] = useState(false);
+  const [filters, setFilters] = useState({
+    interesIds: [],
+    habilidadIds: [],
+  });
 
   return (
     <>
       <div className="feed-container w-full h-full flex flex-col items-center justify-evenly overflow-hidden 2xl:py-10 xl:py-3 xl:gap-4">
-        <TabBar currentTab={currentTab} setCurrentTab={setCurrentTab} />
-        <Feed currentTab={currentTab} setCurrentUserId={setCurrentUserId} />
-        <ActionBar userId={currentUserId} />
+        {applyFilters ? (
+          <>
+            <TabBarByFilters currentTab={currentTab} setCurrentTab={setCurrentTab} />
+            <FeedByFilters currentTab={currentTab} setCurrentUserId={setCurrentUserId} applyFilters={applyFilters} setApplyFilters={setApplyFilters} filters={filters} setFilters={setFilters} />
+          </>
+        ) : (
+          <>
+            <TabBar currentTab={currentTab} setCurrentTab={setCurrentTab} />
+            <Feed currentTab={currentTab} setCurrentUserId={setCurrentUserId} />
+          </>
+        )}
+
+        <ActionBar userId={currentUserId} applyFilters={applyFilters} setApplyFilters={setApplyFilters} filters={filters} setFilters={setFilters} />
       </div>
     </>
   );
